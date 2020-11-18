3dfbe7e80e51a63fad38aa124905bfac671f7d73
SENO AJI NUGROHO <seno.nugroho@binus.ac.id>
2020-11-18 14:47:18 +0700

    add mongoose, mongodb, bodyparser, and short id to package.json update server.js with all functinality for short url update index page to display link to short url tool add information page on url shortening Microservices

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 // server.js
 // where your node app starts
 
+// DataBase URI
+
+var database_uri = 'mongodb+srv://RatHead:admin123@clushead.t9tw1.mongodb.net/<ClusHead>?retryWrites=true&w=majority'
+
 // init project
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var shortid = require('shortid');
 var port = process.env.PORT || 3000
+
+
+mongoose.connect(database_uri, 
+{ 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+});
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -27,6 +42,10 @@ app.get("/requestHeaderParser", function (req, res) {
   res.sendFile(__dirname + '/views/requestHeaderParser.html');
 });
 
+app.get("/urlshortenermicroservices", function (req, res) {
+  res.sendFile(__dirname + '/views/urlshortenermicroservices.html');
+});
+
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
@@ -34,6 +53,7 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//Timestamp Project
 app.get("/api/timestamp", function(req, res)
 {
   var now = new Date()
@@ -71,16 +91,68 @@ app.get("/api/timestamp/:date_string", function(req,res)
   }
 });
 
+// Header Request Project
 app.get("/api/whoami", function(req, res)
   {
     res.json
     ({
       "ipaddress": req.connection.remoteAddress,
       "language": req.headers["accept-language"],
-      "software": req.headers["user-agent"],
-      //"req-headers": req.headers
-    })
+      "software": req.headers["user-agent"]
+    });
   });
+
+  // URL Shortener Project
+  
+  // Build a Schema and model to store saved URLs
+
+  var ShortURL= mongoose.model('ShortURL', new mongoose.Schema({ 
+    short_url: String,
+    original_url: String,
+    suffix: String 
+    
+  }));
+
+  // parse application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: false }))
+ 
+  // parse application/json
+  app.use(bodyParser.json())
+  app.post("/api/shorturl/new", function (req, res) 
+  {
+
+    let client_requested_url = req.body.url
+    let suffix = shortid.generate();
+    let newShortURL = suffix
+
+
+    let newURL = new ShortURL({
+
+      short_url: __dirname + "/api/shorturl/" + suffix,
+      original_url: client_requested_url,
+      suffix: suffix
+    });
+
+    newURL.save(function(err, data)
+      {
+      if(err) return console.error(err);
+      res.json({
+      "saved": true,
+      "short_url": newURL.short_url,
+      "original_url": newURL.original_url,
+      "suffix": newURL.suffix
+      });
+    });
+  });
+
+  app.get("/api/shorturl/:suffix", function(req, res)
+  {
+    let GeneratedSuffix = req.params.suffix
+    ShortURL.find({suffix: GeneratedSuffix}).then(function(foundURLS) {
+      let urlForRedirect = foundURLS[0];
+      res.redirect(urlForRedirect.original_url);
+      });
+    });
 
 // listen for requests :)
 var listener = app.listen(port, function () {
